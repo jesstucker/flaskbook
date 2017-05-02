d81082f66b61ed49ca3fc6dea6effbfe3d1350c1
Jess Tucker <jess.tucker@gmail.com>
2017-05-02 11:52:31 -0700

    annoyed

--- config.py
+++ config.py
@@ -3,34 +3,34 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 
 class Config:
-    # SECRET_KEY = os.environ.get('SECRET_KEY') or 'hard to guess string'
-    # SSL_DISABLE = False
-    # SQLALCHEMY_RECORD_QUERIES = True
-    # CSRF_ENABLED = True
-    # SQLALCHEMY_COMMIT_ON_TEARDOWN = True
-    # SQLALCHEMY_TRACK_MODIFICATIONS = False
-    # MAIL_SERVER = 'smtp.gmail.com'
-    # MAIL_PORT = 587
-    # MAIL_USE_TLS = True
-    # MAIL_USERAME = os.environ.get('MAIL_USERNAME')
-    # MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
-    # FLASKY_MAIL_SUBJECT_PREFIX = '[Flasky]'
-    # FLASKY_MAIL_SENDER = 'jess.tucker@gmail.com'
-    # FLASKY_ADMIN = 'jess.tucker@gmail.com'
-
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'hard to guess string'
     SSL_DISABLE = False
+    SQLALCHEMY_RECORD_QUERIES = True
+    CSRF_ENABLED = True
     SQLALCHEMY_COMMIT_ON_TEARDOWN = True
-    SQLCLCHEMY_RECORD_QUERIES = True
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
     MAIL_SERVER = 'smtp.gmail.com'
-    MAIL_PORT = 465
-    MAIL_USE_TLS = False
-    MAIL_USE_SSL = True
-    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     FLASKY_MAIL_SUBJECT_PREFIX = '[Flasky]'
-    FLASKY_MAIL_SENDER = 'Flasky Admin <jess.tucker@gmail.com>'
-    FLASKY_ADMIN = os.environ.get('FLASKY_ADMIN')
+    FLASKY_MAIL_SENDER = 'jess.tucker@gmail.com'
+    FLASKY_ADMIN = 'jess.tucker@gmail.com'
+
+    # SECRET_KEY = os.environ.get('SECRET_KEY') or 'hard to guess string'
+    # SSL_DISABLE = False
+    # SQLALCHEMY_COMMIT_ON_TEARDOWN = True
+    # SQLCLCHEMY_RECORD_QUERIES = True
+    # MAIL_SERVER = 'smtp.gmail.com'
+    # MAIL_PORT = 465
+    # MAIL_USE_TLS = False
+    # MAIL_USE_SSL = True
+    # MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    # MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    # FLASKY_MAIL_SUBJECT_PREFIX = '[Flasky]'
+    # FLASKY_MAIL_SENDER = 'Flasky Admin <jess.tucker@gmail.com>'
+    # FLASKY_ADMIN = os.environ.get('FLASKY_ADMIN')
 
     @staticmethod
     def init_app(app):
