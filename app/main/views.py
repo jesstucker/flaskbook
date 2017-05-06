72ce0b7fd1f6cecc253c70b21f1fa855f04bf268
Jess Tucker <jess.tucker@gmail.com>
2017-05-06 08:41:19 -0700

    admin woes lol

--- app/main/views.py
+++ app/main/views.py
@@ -1,7 +1,7 @@
 from flask import render_template, session, redirect, url_for, current_app
 from flask_login import login_required, current_user
 from . import main
-from .forms import NameForm
+from .forms import NameForm, EditProfileForm
 from ..email import send_email
 from .. import db
 from ..models import User
