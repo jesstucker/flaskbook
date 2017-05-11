ef1149e3d34856cfab37e591e462d13895ef49d6
Jess Tucker <jess.tucker@gmail.com>
2017-05-11 16:50:24 -0700

    ITW

--- hellos/hello5.py
+++ hellos/hello5.py
@@ -30,7 +30,7 @@ app.config['MAIL_SERVER'] = 'stmp.googlemail.com'
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
 app.config['MAIL_USERNAME'] = os.environ.get('MAIL_USERNAME')
-app.config('MAIL_PASSWORD'] = os.environ.get('MAIL_PASSWORD')
+app.config['MAIL_PASSWORD'] = os.environ.get('MAIL_PASSWORD')
 
 
 
