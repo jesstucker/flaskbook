e3c544d0612144bd18a2178c617de34dba138c5d
Jess Tucker <jess.tucker@gmail.com>
2017-05-16 11:44:18 -0700

    to see if i could replace a database witha text file...yep

--- hellos/hello69/hola.py
+++ hellos/hello69/hola.py
@@ -1,39 +1,39 @@
 from flask import Flask, request
+import os
 app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def form():
     return '''
 		<form method="post" action="/hello">
-
-		  <label for="say">SAY WHAT?</label>
-		  <input name="say">
-
-		  <label for="to">TO WHOM?</label>
-		  <input name="to">
-
+		  <label for="say">SAY WHAT?</label><input name="say">
+		  <label for="to">TO WHOM?</label><input name="to">
 		  <button>Send POST</button>
-
 		</form>
 		'''
 
 @app.route('/hello', methods=['GET', 'POST'])
 def hello():
+    request.get_data(as_text=True)
+    if os.path.exists('database.txt'):
+    	database = open('database.txt', 'a')
+    else:	
+    	database = open('database.txt', 'w')
+    database.write(request.form['say'] + ',' + request.form['to'] + '\n')
+    database = open('database.txt', 'r')
     return \
     '''
       <p>
 	      Say: {0} <br> 
 	      To: {1} <br>
-	      Python Values:{2} <br>
-	      Browser Submitted Values: {3} <br>
-	      Stream attempt: {4}
+	      Form Data Unparsed:{2} <br>
+	      Stored: <br> {3} <br>
       </p>
 	'''.format(\
 		request.form['say'],\
 		request.form['to'], \
-		request.values, \
-		request.get_data(as_text=True),\
-		request.stream.read(),\
+		request.data,\
+		database.read().replace('\n', '<br>')
 		)
 
 
